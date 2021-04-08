36d6fd82c29e0feec7e4ef4bab4b6a842af68fe9
ushair <ushairansari@gmail.com>
2021-04-08 18:32:53 +0530

    y

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export default function App() {
   const [isStyle, setStyle] = useState();
-
+  const [number, setNumber] = useState(0);
   function changestyle() {
     setStyle(!isStyle);
+  }
+  function change() {
+    return setNumber(number + 1);
   }
   const styleheading = {
     textDecoration: "line-through",
     backgroundColor: "blue"
   };
+  useEffect(() => {
+    if (number === 5) alert("hello world");
+  });
+
   return (
     <div className="App">
-      <h1 style={isStyle ? styleheading : null}>Hello CodeSandbox</h1>
+      <h1 style={isStyle ? styleheading : null}>Hello {number} CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={changestyle}>Change Style</button>
+      <button onClick={change}>Change</button>
     </div>
   );
 }
